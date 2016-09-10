92ba2c746e2a8c00b6c27e752b5a5b3c8141572a
Christopher Gosala <cgosala@deloitte.com>
2016-09-10 11:11:23 +0530

    Basic set up for custom component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
+    moduleId: module.id,
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App<custom-tag></custom-tag></h1>'
+    // template: '<h1>My First Angular 2 App<custom-tag></custom-tag></h1>'
+    templateUrl: './app.component.html'
 })
 export class AppComponent { }
--- app/custom/custom.component.ts
+++ app/custom/custom.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
+    // moduleId :: setting moduleId: module.id in the @Component decorator is the key here.
+    //             If you don't have that then Angular 2 will be looking for your files at
+    //             the root level.
+    moduleId: module.id,
+    // Tag for this component
     selector:'custom-tag',
-    template:'<h4>custom mark up</h4>'
+    // Styles to be picked for this component
+    styleUrls: ['./custom.component.css'],
+    // Emplate to be picked for this component
+    templateUrl: './custom.component.html'
 })
 
 export class CustomComponent {};
