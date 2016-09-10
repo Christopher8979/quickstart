38a299f37c6a853c69c1f2fed3bdefc2f7ee5f2b
Christopher Gosala <cgosala@deloitte.com>
2016-09-10 16:23:16 +0530

    Combo box initial commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { ComboBoxComponent } from './combo-box/combobox.component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent, BreadcrumbComponent ],
+  declarations: [ AppComponent, BreadcrumbComponent, ComboBoxComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ app/combo-box/combobox.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    // moduleId :: setting moduleId: module.id in the @Component decorator is the key here.
+    //             If you don't have that then Angular 2 will be looking for your files at
+    //             the root level.
+    moduleId: module.id,
+    // Tag for this component
+    selector:'combo-box',
+    // Styles to be picked for this component
+    styleUrls: ['./combobox.component.css'],
+    // Emplate to be picked for this component
+    templateUrl: './combobox.component.html'
+})
+
+export class ComboBoxComponent implements OnInit {
+
+    // ngOnInit function gets exicuted when component is initiated.
+    ngOnInit(){
+    }
+};
